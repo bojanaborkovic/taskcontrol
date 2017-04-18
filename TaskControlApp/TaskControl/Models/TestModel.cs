c8f244b2793f1be1a90ebe8955c30d4fe12d6f78
Bojana Borković <bojanab@comtrade.com>
2017-04-18 15:23:09 +0200

    adding new model class, testing jenkins build

---
+++ TaskControlApp/TaskControl/Models/TestModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskControl.Models
+{
+	public class TestModel
+	{
+		public TestModel()
+		{
+
+		}
+
+		public string MyProperty { get; set; }
+	}
+}
