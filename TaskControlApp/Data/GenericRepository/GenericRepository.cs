9ef05e8d403129b3b4db6c60b918590d219f037a
bojanab <bojanab@BOJANA-PC>
2017-08-23 16:02:26 +0200

    Fixed code formatting for DataModel project

--- TaskControlApp/Data/GenericRepository/GenericRepository.cs
+++ TaskControlApp/Data/GenericRepository/GenericRepository.cs
@@ -8,159 +8,159 @@ using DataModel;
 
 namespace DataModel
 {
-	/// <summary>
-	/// Generic class for basic entity operations
-	/// </summary>
-	/// <typeparam name="TEntity"></typeparam>
-	public class GenericRepository<TEntity> where TEntity : class
-	{
-		internal TaskControlEntities Context;
-		internal DbSet<TEntity> dbSet;
-		private readonly bool _lazyLoadingEnabled = true;
+  /// <summary>
+  /// Generic class for basic entity operations
+  /// </summary>
+  /// <typeparam name="TEntity"></typeparam>
+  public class GenericRepository<TEntity> where TEntity : class
+  {
+    internal TaskControlEntities Context;
+    internal DbSet<TEntity> dbSet;
+    private readonly bool _lazyLoadingEnabled = true;
 
-		public GenericRepository(TaskControlEntities context)
-		{
-			this.Context = context;
-			this.Context.Configuration.LazyLoadingEnabled = _lazyLoadingEnabled;
-			this.dbSet = context.Set<TEntity>();
-		}
+    public GenericRepository(TaskControlEntities context)
+    {
+      this.Context = context;
+      this.Context.Configuration.LazyLoadingEnabled = _lazyLoadingEnabled;
+      this.dbSet = context.Set<TEntity>();
+    }
 
-		#region public member methods
-		public virtual IEnumerable<TEntity> Get()
-		{
-			IEnumerable<TEntity> query = dbSet;
-			return query.ToList();
-		}
-
-
-		public virtual TEntity GetByID(long Id)
-		{
-			return dbSet.Find(Id);
-		}
-
-		public virtual void Insert(TEntity entity)
-		{
-			dbSet.Add(entity);
-		}
-
-		public virtual void Delete(long Id)
-		{
-			TEntity entityToDelete = dbSet.Find(Id);
-			Delete(entityToDelete);
-		}
-
-		public virtual void Delete(TEntity entity)
-		{
-			if (Context.Entry(entity).State == EntityState.Detached)
-			{
-				dbSet.Attach(entity);
-			}
-
-			dbSet.Remove(entity);
-		}
-
-		public virtual void Update(TEntity entityToUpdate)
-		{
-			dbSet.Attach(entityToUpdate);
-			Context.Entry(entityToUpdate).State = EntityState.Modified;
-		}
-
-		/// <summary>
-		/// generic method that gets many records on the basis of condition
-		/// </summary>
-		/// <param name="where"></param>
-		/// <returns></returns>
-		public virtual IEnumerable<TEntity> GetMany(Func<TEntity, bool> where)
-		{
-			return dbSet.Where(where).ToList();
-		}
+    #region public member methods
+    public virtual IEnumerable<TEntity> Get()
+    {
+      IEnumerable<TEntity> query = dbSet;
+      return query.ToList();
+    }
 
 
-		public virtual IEnumerable<TEntity> GetManyQueryable(Func<TEntity, bool> where)
-		{
-			return dbSet.Where(where).AsQueryable();
-		}
+    public virtual TEntity GetByID(long Id)
+    {
+      return dbSet.Find(Id);
+    }
 
-		/// <summary>
-		/// fetches data for the entities on the basis of condition
-		/// </summary>
-		/// <param name="where"></param>
-		/// <returns></returns>
-		public virtual IEnumerable<TEntity> Get(
-					 Expression<Func<TEntity, bool>> filter = null,
-					 Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
-					 string includeProperties = "")
-		{
-			IQueryable<TEntity> query = dbSet;
+    public virtual void Insert(TEntity entity)
+    {
+      dbSet.Add(entity);
+    }
 
-			if (filter != null)
-			{
-				query = query.Where(filter);
-			}
+    public virtual void Delete(long Id)
+    {
+      TEntity entityToDelete = dbSet.Find(Id);
+      Delete(entityToDelete);
+    }
 
-			foreach (var includeProperty in includeProperties.Split
-					(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
-			{
-				query = query.Include(includeProperty);
-			}
+    public virtual void Delete(TEntity entity)
+    {
+      if (Context.Entry(entity).State == EntityState.Detached)
+      {
+        dbSet.Attach(entity);
+      }
 
-			if (orderBy != null)
-			{
-				return orderBy(query).ToList();
-			}
-			else
-			{
-				return query.ToList();
-			}
-		}
+      dbSet.Remove(entity);
+    }
+
+    public virtual void Update(TEntity entityToUpdate)
+    {
+      dbSet.Attach(entityToUpdate);
+      Context.Entry(entityToUpdate).State = EntityState.Modified;
+    }
+
+    /// <summary>
+    /// generic method that gets many records on the basis of condition
+    /// </summary>
+    /// <param name="where"></param>
+    /// <returns></returns>
+    public virtual IEnumerable<TEntity> GetMany(Func<TEntity, bool> where)
+    {
+      return dbSet.Where(where).ToList();
+    }
 
 
-		/// <summary>
-		/// deletes data for the entities on the basis fo condition
-		/// </summary>
-		/// <param name="where"></param>
-		public void Delete(Func<TEntity, Boolean> where)
-		{
-			IEnumerable<TEntity> objects = dbSet.Where<TEntity>(where).AsQueryable();
-			foreach(TEntity obj in objects)
-			{
-				dbSet.Remove(obj);
-			}
+    public virtual IEnumerable<TEntity> GetManyQueryable(Func<TEntity, bool> where)
+    {
+      return dbSet.Where(where).AsQueryable();
+    }
 
-		}
+    /// <summary>
+    /// fetches data for the entities on the basis of condition
+    /// </summary>
+    /// <param name="where"></param>
+    /// <returns></returns>
+    public virtual IEnumerable<TEntity> Get(
+           Expression<Func<TEntity, bool>> filter = null,
+           Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+           string includeProperties = "")
+    {
+      IQueryable<TEntity> query = dbSet;
 
-		/// <summary>
-		/// fetch all records from the db
-		/// </summary>
-		/// <returns></returns>
+      if (filter != null)
+      {
+        query = query.Where(filter);
+      }
 
-		public virtual IEnumerable<TEntity> GetAll()
-		{
-			return dbSet.ToList();
-		}
+      foreach (var includeProperty in includeProperties.Split
+          (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+      {
+        query = query.Include(includeProperty);
+      }
 
-		/// <summary>
-		/// incldue multiple
-		/// </summary>
-		/// <param name="predicate"></param>
-		/// <param name="include"></param>
-		/// <returns></returns>
-		//public IEnumerable<TEntity> GetWithInclude(System.Linq.Expressions.Expression<Func<TEntity, bool>> predicate, params string[] include)
-		//{
-		//	IEnumerable<TEntity> query = this.DbSet;
-		//	query = include.Aggregate(query, (current, inc) => current.Include(inc));
-		//	return query.Where(predicate);
-		//}
+      if (orderBy != null)
+      {
+        return orderBy(query).ToList();
+      }
+      else
+      {
+        return query.ToList();
+      }
+    }
 
-		/// <summary>
-		/// generic method to check if entity exists
-		/// </summary>
-		/// <param name="primaryKey"></param>
-		/// <returns></returns>
-		public bool Exists(long primaryKey)
-		{
-			return dbSet.Find(primaryKey) != null;
-		}
+
+    /// <summary>
+    /// deletes data for the entities on the basis fo condition
+    /// </summary>
+    /// <param name="where"></param>
+    public void Delete(Func<TEntity, Boolean> where)
+    {
+      IEnumerable<TEntity> objects = dbSet.Where<TEntity>(where).AsQueryable();
+      foreach (TEntity obj in objects)
+      {
+        dbSet.Remove(obj);
+      }
+
+    }
+
+    /// <summary>
+    /// fetch all records from the db
+    /// </summary>
+    /// <returns></returns>
+
+    public virtual IEnumerable<TEntity> GetAll()
+    {
+      return dbSet.ToList();
+    }
+
+    /// <summary>
+    /// incldue multiple
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <param name="include"></param>
+    /// <returns></returns>
+    //public IEnumerable<TEntity> GetWithInclude(System.Linq.Expressions.Expression<Func<TEntity, bool>> predicate, params string[] include)
+    //{
+    //	IEnumerable<TEntity> query = this.DbSet;
+    //	query = include.Aggregate(query, (current, inc) => current.Include(inc));
+    //	return query.Where(predicate);
+    //}
+
+    /// <summary>
+    /// generic method to check if entity exists
+    /// </summary>
+    /// <param name="primaryKey"></param>
+    /// <returns></returns>
+    public bool Exists(long primaryKey)
+    {
+      return dbSet.Find(primaryKey) != null;
+    }
 
     public object GetByID(long userId, long roleId)
     {
@@ -173,22 +173,22 @@ namespace DataModel
     /// <param name="predicate">Criteria to match on</param>
     /// <returns></returns>
     public TEntity GetSingle(Func<TEntity, bool> predicate)
-		{
-			return dbSet.Single<TEntity>(predicate);
-		}
+    {
+      return dbSet.Single<TEntity>(predicate);
+    }
 
 
-		/// <summary>
-		/// The first record matching the specified criteria
-		/// </summary>
-		/// <param name="predicate">Criteria to match on</param>
-		/// <returns></returns>
+    /// <summary>
+    /// The first record matching the specified criteria
+    /// </summary>
+    /// <param name="predicate">Criteria to match on</param>
+    /// <returns></returns>
 
-		public TEntity GetFirst(Func<TEntity, bool> predicate)
-		{
-			return dbSet.First<TEntity>(predicate);
-		}
+    public TEntity GetFirst(Func<TEntity, bool> predicate)
+    {
+      return dbSet.First<TEntity>(predicate);
+    }
 
-		#endregion
-	}
+    #endregion
+  }
 }
--- TaskControlApp/Data/UnitOfWork/UnitOfWork.cs
+++ TaskControlApp/Data/UnitOfWork/UnitOfWork.cs
@@ -8,59 +8,59 @@ using log4net;
 
 namespace DataModel.UnitOfWork
 {
-	public class UnitOfWork : IDisposable
-	{
+  public class UnitOfWork : IDisposable
+  {
 
-		#region private members
-		private TaskControlEntities context = new TaskControlEntities();
-		private GenericRepository<AspNetUser> _userRepository;
-		private GenericRepository<Task> _taskRepostiory;
-		private GenericRepository<Project> _projectRepository;
+    #region private members
+    private TaskControlEntities context = new TaskControlEntities();
+    private GenericRepository<AspNetUser> _userRepository;
+    private GenericRepository<Task> _taskRepostiory;
+    private GenericRepository<Project> _projectRepository;
     private GenericRepository<AspNetRole> _roleRepository;
     private GenericRepository<AspNetUserRole> _usersInRoleRepository;
-		private GenericRepository<IssueType> _issueTypeRepository;
-		private GenericRepository<Status> _statusRepository;
-		private GenericRepository<Priority> _priorityRepository;
-		internal static readonly ILog _log = LogManager.GetLogger(typeof(UnitOfWork));
-		private bool disposed = false;
+    private GenericRepository<IssueType> _issueTypeRepository;
+    private GenericRepository<Status> _statusRepository;
+    private GenericRepository<Priority> _priorityRepository;
+    internal static readonly ILog _log = LogManager.GetLogger(typeof(UnitOfWork));
+    private bool disposed = false;
     #endregion
 
     #region repositories
     public GenericRepository<AspNetUser> UserRepository
-		{
-			get
-			{
-				if(this._userRepository == null)
-				{
-					this._userRepository = new GenericRepository<AspNetUser>(context);
-				}
-				return _userRepository;
-			}
-		}
+    {
+      get
+      {
+        if (this._userRepository == null)
+        {
+          this._userRepository = new GenericRepository<AspNetUser>(context);
+        }
+        return _userRepository;
+      }
+    }
 
-		public GenericRepository<Task> TaskRepository
-		{
-			get
-			{
-				if(this._taskRepostiory == null)
-				{
-					this._taskRepostiory = new GenericRepository<Task>(context);
-				}
-				return _taskRepostiory;
-			}
-		}
+    public GenericRepository<Task> TaskRepository
+    {
+      get
+      {
+        if (this._taskRepostiory == null)
+        {
+          this._taskRepostiory = new GenericRepository<Task>(context);
+        }
+        return _taskRepostiory;
+      }
+    }
 
-		public GenericRepository<Project> ProjectRepository
-		{
-			get
-			{
-				if (this._projectRepository == null)
-				{
-					this._projectRepository = new GenericRepository<Project>(context);
-				}
-				return _projectRepository;
-			}
-		}
+    public GenericRepository<Project> ProjectRepository
+    {
+      get
+      {
+        if (this._projectRepository == null)
+        {
+          this._projectRepository = new GenericRepository<Project>(context);
+        }
+        return _projectRepository;
+      }
+    }
 
     public GenericRepository<AspNetRole> RoleRepository
     {
@@ -86,69 +86,69 @@ namespace DataModel.UnitOfWork
       }
     }
 
-		public GenericRepository<IssueType> IssueTypeRepositorsy
-		{
-			get
-			{
-				if (this._issueTypeRepository == null)
-				{
-					this._issueTypeRepository = new GenericRepository<IssueType>(context);
-				}
-				return _issueTypeRepository;
-			}
-		}
+    public GenericRepository<IssueType> IssueTypeRepositorsy
+    {
+      get
+      {
+        if (this._issueTypeRepository == null)
+        {
+          this._issueTypeRepository = new GenericRepository<IssueType>(context);
+        }
+        return _issueTypeRepository;
+      }
+    }
 
-		public GenericRepository<Status> StatusRepository
-		{
-			get
-			{
-				if (this._statusRepository == null)
-				{
-					this._statusRepository = new GenericRepository<Status>(context);
-				}
-				return _statusRepository;
-			}
-		}
+    public GenericRepository<Status> StatusRepository
+    {
+      get
+      {
+        if (this._statusRepository == null)
+        {
+          this._statusRepository = new GenericRepository<Status>(context);
+        }
+        return _statusRepository;
+      }
+    }
 
-		public GenericRepository<Priority> PriorityRepository
-		{
-			get
-			{
-				if (this._priorityRepository == null)
-				{
-					this._priorityRepository = new GenericRepository<Priority>(context);
-				}
-				return _priorityRepository;
-			}
-		}
-		#endregion
+    public GenericRepository<Priority> PriorityRepository
+    {
+      get
+      {
+        if (this._priorityRepository == null)
+        {
+          this._priorityRepository = new GenericRepository<Priority>(context);
+        }
+        return _priorityRepository;
+      }
+    }
+    #endregion
 
-		#region public member methods
+    #region public member methods
 
-		public void Save()
-		{
-			try
-			{
-				context.SaveChanges();
-			}
-			catch(DbEntityValidationException dbE)
-			{
-				_log.ErrorFormat("Error ocurred during saving changes in the database: {0} ", dbE.Message);
-				var outputLines = new List<string>();
-				foreach (var eve in dbE.EntityValidationErrors)
-				{
-					outputLines.Add(string.Format(
-							"{0}: Entity of type \"{1}\" in state \"{2}\" has the following validation errors:", DateTime.Now,
-							eve.Entry.Entity.GetType().Name, eve.Entry.State));
-					foreach (var ve in eve.ValidationErrors)
-					{
-						outputLines.Add(string.Format("- Property: \"{0}\", Error: \"{1}\"", ve.PropertyName, ve.ErrorMessage));
-					}
+    public void Save()
+    {
+      try
+      {
+        context.SaveChanges();
+      }
+      catch (DbEntityValidationException dbE)
+      {
+        _log.ErrorFormat("Error ocurred during saving changes in the database: {0} ", dbE.Message);
+        var outputLines = new List<string>();
+        foreach (var eve in dbE.EntityValidationErrors)
+        {
+          outputLines.Add(string.Format(
+              "{0}: Entity of type \"{1}\" in state \"{2}\" has the following validation errors:", DateTime.Now,
+              eve.Entry.Entity.GetType().Name, eve.Entry.State));
+          foreach (var ve in eve.ValidationErrors)
+          {
+            outputLines.Add(string.Format("- Property: \"{0}\", Error: \"{1}\"", ve.PropertyName, ve.ErrorMessage));
+          }
 
-					_log.ErrorFormat("Errors: {0}", outputLines);
-				}
-			}
-		}
+          _log.ErrorFormat("Errors: {0}", outputLines);
+        }
+      }
+    }
     #endregion
 
 
@@ -164,60 +164,60 @@ namespace DataModel.UnitOfWork
       }
     }
 
-		public List<ProjectResult> GetProjectsWithOwner()
-		{
-			using (var context = new TaskControlEntities())
-			{
-				var projects = context.GetAllProjectsWithOwner();
+    public List<ProjectResult> GetProjectsWithOwner()
+    {
+      using (var context = new TaskControlEntities())
+      {
+        var projects = context.GetAllProjectsWithOwner();
 
-				return projects.ToList();
-			}
-		}
+        return projects.ToList();
+      }
+    }
 
 
-		public List<TaskDetailsResult> GetAllTasksDetails()
-		{
-			using (var context = new TaskControlEntities())
-			{
-				var tasks = context.GetAllTasksDetails();
+    public List<TaskDetailsResult> GetAllTasksDetails()
+    {
+      using (var context = new TaskControlEntities())
+      {
+        var tasks = context.GetAllTasksDetails();
 
-				return tasks.ToList();
-			}
-		}
+        return tasks.ToList();
+      }
+    }
 
-		public List<GetTaskResult> GetTaskById(long taskId)
-		{
-			using (var context = new TaskControlEntities())
-			{
-				var task = context.GetTaskById(taskId).ToList();
+    public List<GetTaskResult> GetTaskById(long taskId)
+    {
+      using (var context = new TaskControlEntities())
+      {
+        var task = context.GetTaskById(taskId).ToList();
 
-				return task;
-			}
-		}
+        return task;
+      }
+    }
 
-		#endregion
+    #endregion
 
-		#region IDisposable implementation
+    #region IDisposable implementation
 
-		protected virtual void Dispose(bool disposing)
-		{
-			if(!this.disposed)
-			{
-				if(disposing)
-				{
-					_log.DebugFormat("UnitOfWork is being disposed");
-					context.Dispose();
-				}
-			}
-			this.disposed = true;
-		}
+    protected virtual void Dispose(bool disposing)
+    {
+      if (!this.disposed)
+      {
+        if (disposing)
+        {
+          _log.DebugFormat("UnitOfWork is being disposed");
+          context.Dispose();
+        }
+      }
+      this.disposed = true;
+    }
 
-		public void Dispose()
-		{
-			Dispose(true);
-			GC.SuppressFinalize(this); // instruction to garbage collector 
-		}
+    public void Dispose()
+    {
+      Dispose(true);
+      GC.SuppressFinalize(this); // instruction to garbage collector 
+    }
 
-		#endregion
-	}
+    #endregion
+  }
 }
